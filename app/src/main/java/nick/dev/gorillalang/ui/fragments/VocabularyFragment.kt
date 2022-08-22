2cde3e67d599f9e6d9892831f4a12d88290e5c9b
Nick <zenevichnikita2001@gmail.com>
2022-08-22 12:49:58 +0200

    Added navigation graph

--- app/src/main/java/nick/dev/gorillalang/ui/fragments/VocabularyFragment.kt
+++ app/src/main/java/nick/dev/gorillalang/ui/fragments/VocabularyFragment.kt
@@ -1,10 +1,12 @@
 package nick.dev.gorillalang.ui.fragments
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.view.ViewGroup
 import androidx.lifecycle.Observer
-import androidx.lifecycle.viewModelScope
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.LinearLayoutManager
 import nick.dev.gorillalang.R
 import nick.dev.gorillalang.adapters.ModuleAdapter
@@ -13,10 +15,21 @@ import nick.dev.gorillalang.ui.MainActivity
 import nick.dev.gorillalang.ui.viewModels.LanguageViewModel
 
 
-class VocabularyFragment : Fragment(R.layout.fragment_vocabulary) {
+class VocabularyFragment : Fragment() {
     lateinit var languageViewModel: LanguageViewModel
     lateinit var moduleAdapter: ModuleAdapter
     private lateinit var binding: FragmentVocabularyBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_vocabulary, container,false)
+
+        return view
+    }
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,8 +37,7 @@ class VocabularyFragment : Fragment(R.layout.fragment_vocabulary) {
         binding = FragmentVocabularyBinding.bind(view)
         setupRecyclerView()
         binding.btnAddModule.setOnClickListener {
-            val addModuleFragment = AddModuleFragment()
-            (activity as (MainActivity)).setCurrentFragment(addModuleFragment)
+            Navigation.findNavController(view).navigate(R.id.navigateToAddModuleFromVocabulary)
         }
         languageViewModel.getUserModules().observe(viewLifecycleOwner, Observer {
             moduleAdapter.differ.submitList(it)
